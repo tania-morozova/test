4add21fb62150d6893d087dc9ee1e7b10ad94b42
Tmoroz0077 <tmoroz0077@gmail.com>
2018-02-27 10:19:16 +0300

    +const

--- task1_fin.py
+++ task1_fin.py
@@ -1,6 +1,6 @@
 import re, os
 
-f = "ex2.tex"
+f = "ex1.tex"
 f_copy = re.subn("\.","_copy.",f,1)[0]
 
 o = open(f_copy,"wb")
@@ -21,7 +21,7 @@ subs3 = "\\\\ldots "
 regexp4 = r"(?P<fn>[А-Я][а-я]+)\-(?P<sn>[А-Я][а-я]+)"   #+
 subs4 = "\g<fn>--\g<sn>"
 
-regexp5 = r"([^\w\\]|^|\d|_)(max|min|sup|lim|inf|sin|\
+regexp5 = r"([^\w\\]|^|\d|_)(const|max|min|sup|lim|inf|sin|\
                 cos|tan|sec|csc|cot|arg|sinh|cosh|tanh|coth|\
                 exp|ker|dim|arcsin|arccos|arctan|log|ln|lg|\
                 deg|det|tg|ctg|cth|sh|ch|th|cosec|arctg)([^\w]|$|\d|_)"
@@ -33,12 +33,12 @@ subs6 = r"$\g<1>\\left<\g<2>,\g<3>\\right>\g<4>$"
 regexp7 = r"\$\$([s|\S]*?)\(([s|\S]*?)\\frac([s|\S]*?)\)([s|\S]*?)\$\$"
 subs7 = r"$$\g<1>\\left(\g<2>\\frac\g<3>\\right)\g<4>$$"    #+
 
-##text = re.sub(regexp1,subs1,text)
-##text = re.sub(regexp3,subs3,text)
-##text = re.sub(regexp4,subs4,text)
-##text = re.sub(regexp5,subs5,text)
-##text = re.sub(regexp6,subs6,text)
-##text = re.sub(regexp7,subs7,text)
+text = re.sub(regexp1,subs1,text)
+text = re.sub(regexp3,subs3,text)
+text = re.sub(regexp4,subs4,text)
+text = re.sub(regexp5,subs5,text)
+text = re.sub(regexp6,subs6,text)
+text = re.sub(regexp7,subs7,text)
 text = re.sub(regexp2,subs2,text)
 
 o.write(bytes(text,'utf-8'))
